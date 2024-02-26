2a7e46fcae8ad0242b10bdd8812d13b4287e7ba4
fardanhafidz <muhfardanhafidz@gmail.com>
2024-02-26 08:34:26 +0700

    First Commit

---
+++ database/migrations/2024_02_23_013444_create_items_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('size')->nullable();
+            $table->integer('price');
+            $table->longText('decsription');
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
